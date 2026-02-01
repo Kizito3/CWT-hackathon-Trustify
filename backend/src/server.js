eee0bce7bd27c490ec4e9ffe80ca3ec8e3b077a8
okwara chigozirim kizito <chigokizzy@gmail.com>
2026-02-01 03:53:43 +0100

    fix:added vercel domain to CORS

--- backend/src/server.js
+++ backend/src/server.js
@@ -21,6 +21,7 @@ app.use(
       "http://localhost:5173",
       "https://cwt-hackathon-trustify.onrender.com",
       "https://cwt-hackathon-trustify-nxoz22m92-kizito3s-projects.vercel.app/",
+      "https://cwt-hackathon-trustify.vercel.app/",
     ],
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
